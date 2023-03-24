9bf54c8c2785471fb300a4d5f110767454e55612
cotwelf <812811926@qq.com>
2023-03-24 14:56:52 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: resolve(__dirname, ''),
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'covid-19-snake/index.html'),
         haru: resolve(__dirname, 'haru-catch/index.html')
       }
     }
